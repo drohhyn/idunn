4c2da525e229281a470e2ba35d3a60899a0f84ad
drohhyn <drohhyn@qimp.org>
2017-12-10 11:45:20 +0100

    added config with sample including db path and openweather api keys

---
+++ pith/config.sample.php
@@ -0,0 +1,9 @@
+<?php
+$CONFIG = array(
+    'db.path' => 'pith.sl3',
+    'openweather.api' => array(
+        'id' => '1234567',
+        'appid' => '0c0ff33c0ff33c0ff33c0ff33c0ff330',
+        'units' => 'metric',
+    ),
+);
