0f20358d65ab9e2653e249493efdc75ed335cb60
drohhyn <drohhyn@qimp.org>
2017-12-10 11:44:46 +0100

    checkHumidity.js for Tasker android app (has to be placed on phone manually, see upconfig README)

---
+++ pith/checkHumidity.js
@@ -0,0 +1,16 @@
+var result = false;
+
+var values = JSON.parse(global('HTTPD')).data;
+
+var cnt = 0;
+values.forEach(function(entry) {
+    if(entry.humidity > 59) {
+        cnt++;
+    }
+});
+
+if(cnt >= 3) {
+    result = true;
+}
+
+setGlobal("%AIROUT", result);
